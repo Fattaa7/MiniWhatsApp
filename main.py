3fa585eb09704ccdfcc24826d396d96aa84d6837
Ahmed ABDELFATTAH-SOLIMAN <ahmed.abdelfattah-soliman@valeo.com>
2023-12-31 17:34:13 +0200

    Sending Message functionality and code refactoring

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import boto3
-import vlc
 import time
-import keyboard
-import urllib
-import socket
-import os
-import re
-
+import sheets
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -26,15 +19,81 @@ def write_list_to_file(file_path, data_list):
             except UnicodeEncodeError:
                 print("Unable to print element due to encoding issues.", file=output_file)
     
+def get_chat_and_msg():
+    chat = ""
+    my_msg = ""
+    with open("write_CMD.txt", "r", encoding="utf-8") as file:
+        line = file.readline()
+    with open("write_CMD_chatName.txt", "r", encoding="utf-8") as file:
+        chatName = file.readline()
+    with open("write_CMD_date.txt", "r", encoding="utf-8") as file:
+        date = file.readline()
 
-service =  Service(executable_path=r"C:\Users\ahmed\Desktop\WhatsScrap\MiniWhatsApp\geckodriver.exe")
+    # Split the line into two values at the first |
+    values = line.strip().split('|', 1)
+    chat, my_msg = values
 
-driver = webdriver.Firefox(service=service)
+    print("Variable 1:", chat)
+    print("Variable 2:", my_msg)
+
+    return chat, my_msg, chatName, date
+
+
+def click_on_chat(chat,chatName,date):
+    # We then try to find the element using the chat name instead
+    # this too could fail if there is emojis in the name
+    # or if the name is in arabic letter -- still not sure about this
+    try:
+        xpath_expression = f"//span[@title='{chatName}']"
+        driver.find_element(By.XPATH, xpath_expression).click()
+
+    except NoSuchElementException:
+        # Try to find the element using the last text appearing on chat from outside
+        # this could fail when there is emojis in the text
+        try:
+            xpath_expression = f"//span[text()='{chat}']"
+            driver.find_element(By.XPATH, xpath_expression).click()
+
+        # for a third try we try to find it using the date that we have stored for the last message send from our friend
+        # this too could have some problems if there are multiple last messages with the same time
+        except NoSuchElementException:
+            xpath_expression = f"//span[text()='{date}']"
+            driver.find_element(By.XPATH, xpath_expression).click()
+
+
+def write_to_chat_and_send(my_msg):
+    actions = ActionChains(driver)
+    #actions.send_keys(my_msg, Keys.RETURN)
+    actions.send_keys(my_msg)
+    actions.perform()
+
+def reset_WCMD_contents():
+    delete_file_contents("write_CMD.txt")
+    delete_file_contents("write_CMD_chatName.txt")
+    delete_file_contents("write_CMD_date.txt")
+
+
+def delete_file_contents(file_name):
+    open(file_name, "w", encoding="utf-8").close()
+
+
+##################################################################################################################################
+######################################## Code start ##############################################################################
+##################################################################################################################################
+
+#PC verison
+#service =  Service(executable_path=r"C:\Users\ahmed\Desktop\WhatsScrap\MiniWhatsApp\geckodriver.exe")
+#driver = webdriver.Firefox(service=service)
+
+# Work version
+service = Service(executable_path=r"C:\Users\aabdelf5\Desktop\PersonalProjects\MiniWhatsApp\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
 driver.get('https://web.whatsapp.com/')
-
+sheets.init()
 driver.implicitly_wait(50)
-
 first_iteration = True
+
 
 while True:
 
@@ -43,78 +102,29 @@ while True:
     sender_list = []
     date_list = []
 
-    # Loop from 1 to 5
-
     if not first_iteration:
-        # Read the content of the text file
-        with open("write_CMD.txt", "r", encoding="utf-8") as file:
-            line = file.readline()
+        msg_available = sheets.readMyMsgAtCol(5)
 
-        # Split the line into two values at the first space
-        values = line.strip().split('|', 1)
-
-        # Check if there are exactly two values
-        if len(values) == 2:
-            chat, my_msg = values
-            print("Variable 1:", chat)
-            print("Variable 2:", my_msg)
-        else:
-            print("Invalid format in the text file.")
-
-        # Replace 'your_title_value' with the actual value you want to search for
-        title_value_to_search = chat
-
-        # Construct the XPath using the title attribute
-        xpath_expression = f"//span[@title='{title_value_to_search}']"
-
-        # Find the element using the constructed XPath
-        driver.find_element(By.XPATH, xpath_expression).click()
-
-        time.sleep(4)
-        
-        # Create an ActionChains object
-        actions = ActionChains(driver)
-
-        # Type your message
-        actions.send_keys(my_msg, Keys.RETURN)
-
-        # Press Enter
-        #actions.send_keys(msg, Keys.RETURN)  # Include any other keys you want to simulate
-
-        # Perform the actions
-        actions.perform()
-
-        # # Replace 'your_class_name' with the actual class name you want to search for
-        # class_name_to_search = "to2l77zo"
-
-        # # Find the element using the class name with XPath
-        # xpath_expression = f"/html/body/div[1]/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div[2]/div[1]"
-        # # Find the element using the constructed XPath
-        # driver.find_element(By.XPATH, xpath_expression).send_keys(my_msg)
-        
-        
-        # data_icon_value_to_search = "send"
-
-        # # Construct the XPath using the data-icon attribute
-        # xpath_expression = f"//span[@data-icon='{data_icon_value_to_search}']"
-
-        # # Find the element using the constructed XPath
-        # driver.find_element(By.XPATH, xpath_expression).click()
-
-
-
+        if msg_available:
+            write_CMD_info = get_chat_and_msg()
     
+            #chat header - the message to be sent - person's name - date
+            chat, my_msg, chatName, date = write_CMD_info
+    
+            click_on_chat(chat,chatName,date)
+            time.sleep(4)
+            write_to_chat_and_send(my_msg)
+            # Delete contents from the write_CMD files to wait for another call by writing to it.
+            reset_WCMD_contents()
+            #sheets.delete_col(5)
+
     
     for i in range(1, 12):
-        # Construct the XPath dynamically
         xpath_person = f"/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[{i}]/div/div/div/div[2]/div[1]/div[1]/div/span"
         xpath_group = f"/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[{i}]/div/div/div/div[2]/div[1]/div[1]/span"
-
         xpath_person_msg = f"/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[{i}]/div/div/div/div[2]/div[2]/div[1]/span/span"
         xpath_group_msg = f"/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[{i}]/div/div/div/div[2]/div[2]/div[1]/span/span[2]"
-
         xpath_sender = f"/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[{i}]/div/div/div/div[2]/div[2]/div[1]/span/div/span"
-
         xpath_date = f"/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[{i}]/div/div/div/div[2]/div[1]/div[2]/span"
 
         if i == 2:
@@ -125,18 +135,19 @@ while True:
             msg = driver.find_element(By.XPATH, xpath_person_msg)
             sender = element
             print(f"found element {i}")
+
+        # If the person XPath fails, try the group XPath
         except NoSuchElementException:
-            # If the person XPath fails, try the group XPath
             try:
                 element = driver.find_element(By.XPATH, xpath_group)
                 msg = driver.find_element(By.XPATH, xpath_group_msg)
                 sender = driver.find_element(By.XPATH, xpath_sender)
                 print(f"found group {i}")
+
+            # Handle the case when both XPaths fail, e.g., raise an error or log a message
             except NoSuchElementException:
-                # Handle the case when both XPaths fail, e.g., raise an error or log a message
                 print(f"Element not found for both XPaths at index {i}")
                 continue  # Skip to the next iteration
-
 
         date = driver.find_element(By.XPATH, xpath_date)
 
@@ -146,60 +157,15 @@ while True:
         date_list.append(date.text)
         sender_list.append(sender.text)
 
-
-    # Example usage:
     write_list_to_file("output.txt", elements_list)
     write_list_to_file("msg.txt", message_list)
     write_list_to_file("sender.txt", sender_list)
     write_list_to_file("date.txt", date_list)
 
-    with open("sheets.py") as f:
-        exec(f.read())
-
+    sheets.write_sheet()
+    
     first_iteration = False
+
+    # This is the sleep before checking for msg_CMD it should be about 40 or 50 seconds but i'll see. 
     time.sleep(10)
-
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[5]/div/div/div/div[2]/div[1]/div[1]/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[10]/div/div/div/div[2]/div[1]/div[1]/div/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[9]/div/div/div/div[2]/div[1]/div[1]/div/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[8]/div/div/div/div[2]/div[1]/div[1]/div/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[7]/div/div/div/div[2]/div[1]/div[1]/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[6]/div/div/div/div[2]/div[1]/div[1]/span
-
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[11]/div/div/div/div[2]/div[1]/div[1]/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[10]/div/div/div/div[2]/div[1]/div[1]/span
-
-
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[11]/div/div/div/div[2]/div[2]/div[1]/span/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[8]/div/div/div/div[2]/div[2]/div[1]/span/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[7]/div/div/div/div[2]/div[2]/div[1]/span/span
-
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[1]/span/span[2]
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[10]/div/div/div/div[2]/div[2]/div[1]/span/span[2]
-
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[10]/div/div/div/div[2]/div[2]/div[1]/span/div/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[1]/span/div[1]/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[17]/div/div/div/div[2]/div[2]/div[1]/span/div/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[10]/div/div/div/div[2]/div[2]/div[1]/span/div/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[9]/div/div/div/div[2]/div[2]/div[1]/span/div/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[8]/div/div/div/div[2]/div[2]/div[1]/span/div/span
-#/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[1]/span/div/span
-
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[11]/div/div/div/div[2]/div[1]/div[2]/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[17]/div/div/div/div[2]/div[1]/div[2]/span
-
-
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div/div/div[2]/div[1]/div[2]/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[10]/div/div/div/div[2]/div[1]/div[2]/span
-
-
-# # UNREAD
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/span[1]/div[2]/span
-
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/span[1]/div[2]/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[11]/div/div/div/div[2]/div[2]/div[2]/span[1]/div[2]/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[7]/div/div/div/div[2]/div[2]/div[2]/span[1]/div[2]/span
-# /html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[10]/div/div/div/div[2]/div[2]/div[2]/span[1]/div/span
-# /html/body/div[1]/div/span[5]
 
--- sheets.py
+++ sheets.py
@@ -1,71 +1,103 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Set up Google Sheets API credentials
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
-client = gspread.authorize(creds)
 
-# Open the Google Sheet by title
-sheet_title = "Whats"
-sheet = client.open(sheet_title).sheet1  # Use the appropriate sheet index if not the first sheet
+def init():
+    global sheet
+    # Set up Google Sheets API credentials
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+    client = gspread.authorize(creds)
 
-# Read data from external files
-with open("output.txt", "r", encoding="utf-8") as file:
-    output_data = [line.strip() for line in file.readlines()]
+    # Open the Google Sheet by title
+    sheet_title = "Whats"
+    sheet = client.open(sheet_title).sheet1  # Use the appropriate sheet index if not the first sheet
 
-with open("msg.txt", "r", encoding="utf-8") as file:
-    msg_data = [line.strip() for line in file.readlines()]
 
-with open("sender.txt", "r", encoding="utf-8") as file:
-    sender_data = [line.strip() for line in file.readlines()]
+def write_sheet():
+    global sorted_data
+    # Read data from external files
+    with open("output.txt", "r", encoding="utf-8") as file:
+        output_data = [line.strip() for line in file.readlines()]
 
-with open("date.txt", "r", encoding="utf-8") as file:
-    date_data = [line.strip() for line in file.readlines()]
+    with open("msg.txt", "r", encoding="utf-8") as file:
+        msg_data = [line.strip() for line in file.readlines()]
 
-# Ensure both lists have the same length
-if len(output_data) != len(msg_data) != len(sender_data) != len(date_data):
-    raise ValueError("All lists must have the same length")
+    with open("sender.txt", "r", encoding="utf-8") as file:
+        sender_data = [line.strip() for line in file.readlines()]
 
-# Combine data into a list of lists
-combined_data = list(zip(output_data, msg_data, sender_data, date_data))
+    with open("date.txt", "r", encoding="utf-8") as file:
+        date_data = [line.strip() for line in file.readlines()]
 
-# Sort combined data based on the "Date" column (assuming "Date" is the fourth column, index 3)
-sorted_data = sorted(combined_data, key=lambda x: x[3], reverse=True)
+    # Ensure both lists have the same length
+    if len(output_data) != len(msg_data) != len(sender_data) != len(date_data):
+        raise ValueError("All lists must have the same length")
 
-# Insert header row
-sorted_data.insert(0, ["Name", "Msg", "Sender", "Date"])
+    # Combine data into a list of lists
+    combined_data = list(zip(output_data, msg_data, sender_data, date_data))
 
-# Clear the existing data in the sheet
+    # Sort combined data based on the "Date" column (assuming "Date" is the fourth column, index 3)
+    sorted_data = sorted(combined_data, key=lambda x: x[3], reverse=True)
+
+    # Insert header row
+    sorted_data.insert(0, ["Name", "Msg", "Sender", "Date"])
+
+    sheet.update("A1:E" + str(len(sorted_data)), sorted_data)
+
+    # Clear the existing data in the sheet
 #sheet.clear()
 
-# Update the sheet with the sorted data
-sheet.update("A1:E" + str(len(sorted_data)), sorted_data)
 
-# Specify the column to search (let's say column E)
-column_to_search = 5
+def readMyMsgAtCol(column_to_search):
 
-# Loop over each row in the specified column until finding the first non-empty cell
-row = 1
-cell = None
+    # Update the sheet with the sorted data
+    
+    # Loop over each row in the specified column until finding the first non-empty cell
+    row = 1
+    cell = None
 
-while not cell:
-    # Find the cell in the current row
-    cell = sheet.cell(row, column_to_search)
+    while not cell:
+        # Find the cell in the current row
+        cell = sheet.cell(row, column_to_search)
 
-    # Check if the cell is empty
-    if not cell.value:
-        cell = None  # Reset cell to None if the cell is empty
-        row += 1  # Move to the next row
+        # Check if the cell is empty
+        if not cell.value:
+            cell = None  # Reset cell to None if the cell is empty
+            row += 1  # Move to the next row
 
-# Prepare the information as a string
-cell_info = f"The first non-empty cell in column {column_to_search} is at row {cell.row}, column {cell.col}, and has the value: {cell.value}"
+    # Prepare the information as a string
+    cell_info = f"The first non-empty cell in column {column_to_search} is at row {cell.row}, column {cell.col}, and has the value: {cell.value}"
 
-cell_chat = sheet.cell(cell.row,1)
+    cell_chat = sheet.cell(cell.row,2)
 
-# Print the information to the console
-print(cell_info)
+    # Print the information to the console
+    print(cell_info)
 
-# Write the information to a text file
-with open("write_CMD.txt", "w", encoding="utf-8") as file:
-    file.write(str(cell_chat.value) + "|" + str(cell.value))
+    # Write the information to a text file
+    # This writes the last text appearing on a chat and the message that we want to send
+    with open("write_CMD.txt", "w", encoding="utf-8") as file:
+        file.write(str(cell_chat.value) + "|" + str(cell.value))
+
+
+    cell_chatName = sheet.cell(cell.row,1)
+    cell_date = sheet.cell(cell.row,4)
+
+    # This writes the chat name to a file in case that the last text appearing has some emojis or something weird in it
+    # so we have 2 ways to search for our wanted chat
+    with open("write_CMD_chatName.txt", "w", encoding="utf-8") as file:
+        file.write(str(cell_chatName.value))
+
+
+    with open("write_CMD_date.txt", "w", encoding="utf-8") as file:
+        file.write(str(cell_date.value))
+
+    if cell.value == None:
+        return False
+    else:
+        return True
+
+def delete_col(col):
+    num_rows = sheet.row_count
+
+    for i in num_rows:
+        sheet.update_cell(i,col, '')
